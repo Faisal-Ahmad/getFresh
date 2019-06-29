cb0cf96de9c4476db096602ab4f476f144f2578f
Tanjil Ahmed <tanzil.ovi578@gmail.com>
2019-06-29 17:08:36 +0600

    login.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+//declaration
 var express= require('express');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 var expressSession = require('express-session');
 var cookieParser = require('cookie-parser');
-
+var login = require('./controllers/login')
 var app = express();
 
+//configuration
 app.set('view engine', 'ejs');
 
+//middleware
+app.use(bodyParser.urlencoded({'entended':false}));
+app.use(expressSession({secret:'my top secret password', saveUninitialized: true, resave: false}));
+app.use(cookieParser());
+
+app.use('login',login);
+
+//routing
 app.get('/', function(req, res){
 	res.send('Welcome to express server...');
 });
