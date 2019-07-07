81769d79e685790bd13038f97860543285e83614
Faisal-Ahmed <faisal152.fa@gmail.com>
2019-07-07 09:37:51 +0600

    customer

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,15 +1,35 @@
 var express = require('express');
+var customer	= require.main.require('./models/customer-model');
 var router = express.Router();
 
 // ##### add item to cart
 
-router.get('/cart/:id', function(req, res){
-    return res.send("add item to cart");
+router.get('/', function(req, res){
+    /*if(req.session.id)
+    {
+        res.render('customer/index');
+    }
+    else
+    {
+        res.redirect('/login');
+    }*/
+    customer.getById('1', function(result){
+		if(result != null){
+			res.render('customer/index', {user: result[0]});		
+		}else{
+			res.send('Error!.. try again...');
+		}
+        });
 });
+router.get('/view_product',function(req,res){
 
-// ##### purchase items
-router.get('/make-purchase', function(req, res){
-    return res.send("make purchase");
+    customer.getAll(function(results){
+		if(results != null){
+			res.render('customer/view_product', {productlist: results});			
+		}else{
+			res.send('Error!.. try again...');
+		}
+    });
 });
 
 module.exports = router;
